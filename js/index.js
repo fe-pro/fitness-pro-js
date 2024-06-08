80214ae8d8769521800e468d5f9d96f01b0b10b9
Breno Cupertino <brenocuper+fep@gmail.com>
2024-06-08 12:52:45 -0300

    feat: sobre json

--- js/index.js
+++ js/index.js
@@ -3,9 +3,24 @@ const form = document.querySelector('form')
 form.addEventListener('submit', function(event) {
     event.preventDefault()
 
-    const email = document.querySelector('#email')
-    const password = document.querySelector('#password')
+    const user = {
+        email: document.querySelector('#email').value,
+        password: document.querySelector('#password').value
+    }
 
-    console.dir(email.value)
-    console.dir(password.value)
+    console.log(user)
+    console.log(
+        JSON.stringify(user)
+    )
+
+    // JavaScript Object Notation = JSON
+
+    /**
+     * JSON é um formato de texto padrão para
+     * representar dados estruturados, usado
+     * principalmente para a troca de dados entre um
+     * servidor e uma aplicação web.
+     */
+
+    // enviarParaOBackEnd(user)
 })
