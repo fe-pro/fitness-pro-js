d1838bbf828c5c52ce2e7a181a3d6d0974cd19e4
Breno Cupertino <brenocuper+fep@gmail.com>
2024-06-30 19:29:49 -0300

    feat: fetch criar conta

--- js/create-account.js
+++ js/create-account.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form')
 
-form.addEventListener('submit', function(event) {
+form.addEventListener('submit', async function(event) {
     event.preventDefault()
 
     const newUser = {
@@ -10,4 +10,20 @@ form.addEventListener('submit', function(event) {
     }
 
     console.log(newUser)
+
+    // Rota: /users
+
+    const url = 'http://127.0.0.1:3333/users'
+
+    const requestData ={
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newUser)
+    }
+
+    const response = await fetch(url, requestData)
+    const data = await response.json()
+    console.log(data)
 })
