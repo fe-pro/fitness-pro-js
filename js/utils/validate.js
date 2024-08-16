36ab68f75a2fc35f26184945aaf4212218a9e680
Breno Cupertino <brenocuper+fep@gmail.com>
2024-08-16 12:10:08 -0300

    refact: encurta metodos em validatejs

--- js/utils/validate.js
+++ js/utils/validate.js
@@ -5,45 +5,52 @@ export const validate = {
     const isValid = validator(inputValue)
     return isValid
   },
-
   displayErrorMessage: ({ inputElement, isValid, errorContainer, errorMessage }) => {
-
     inputElement.classList.toggle('invalid', !isValid)
-  
-    isValid === true
-        ? errorContainer.textContent = ''
-        : errorContainer.textContent = errorMessage
+    errorContainer.textContent = isValid ? '' : errorMessage
   }
 }
 
 export const validators = {
 
-  required: (value) => {
-    if(value !== '') {
-      return true
-    }
-    return false
-  },
-
-  email: (value) => {
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-    if(emailRegex.test(value)) {
-      return true
-    }
-    return false
-  },
-
-  minSixChar: (value) => {
-    if(value.length >= 6) {
-      return true
-    }
-    return false
-  },
-
-  compare: (a, b) => {
-    if(a === b && a !== '') {
-      return true
-    }
-    return false
-  }
+  required: (value) => value !== '',
+  email: (value) => /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value),
+  minSixChar: (value) => value.length >= 6,
+  compare: (a, b) => a === b && a !== ''
 }
+
+// isValid
+//     ? errorContainer.textContent = ''
+//     : errorContainer.textContent = errorMessage
+
+// export const validators = {
+
+//   required: (value) => {
+//     if(value !== '') {
+//       return true
+//     }
+//     return false
+//   },
+
+//   email: (value) => {
+//     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+//     if(emailRegex.test(value)) {
+//       return true
+//     }
+//     return false
+//   },
+
+//   minSixChar: (value) => {
+//     if(value.length >= 6) {
+//       return true
+//     }
+//     return false
+//   },
+
+//   compare: (a, b) => {
+//     if(a === b && a !== '') {
+//       return true
+//     }
+//     return false
+//   }
+// }
