64f7c99d3af833c3784e4e1b3c087dc511333561
Breno Cupertino <brenocuper+fep@gmail.com>
2024-08-16 11:03:40 -0300

    refact: cria validators em validatejs

--- js/utils/validate.js
+++ js/utils/validate.js
@@ -9,26 +9,53 @@ export function displayErrorMessage({ inputElement, isValid, errorContainer, err
 
 export function validateName(nameInput) {
 
-  const isNameValid = nameInput.value.trim() !== '' ? true : false
+  const isNameValid = validators.required(nameInput.value.trim())
   return isNameValid
 }
 
 export function validateEmail(emailInput) {
 
-  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-  const isEmailValid = emailRegex.test(emailInput.value.trim())
-
+  const isEmailValid = validators.email(emailInput.value.trim())
   return isEmailValid
 }
 
 export function validatePassword(passwordInput) {
 
-  const isPasswordValid = passwordInput.value.trim().length >= 6 ? true : false
+  const isPasswordValid = validators.minSixChar(passwordInput.value.trim())
   return isPasswordValid
 }
 
 export function validateConfirmPassword(passwordInput, confirmPasswordInput) {
   
-  const isPasswordEqual = (passwordInput.value === confirmPasswordInput.value && passwordInput.value.trim() !== '') ? true : false
+  const isPasswordEqual = validators.compare(passwordInput.value, confirmPasswordInput.value)
   return isPasswordEqual
+}
+
+const validators = {
+
+  required: (value) => {
+    if(value !== '') {
+      return true
+    }
+    return false
+  },
+  email: (value) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    if(emailRegex.test(value)) {
+      return true
+    }
+    return false
+  },
+  minSixChar: (value) => {
+    if(value.length >= 6) {
+      return true
+    }
+    return false
+  },
+  compare: (a, b) => {
+    if(a === b && a.trim() !== '') {
+      return true
+    }
+    return false
+  }
 }
