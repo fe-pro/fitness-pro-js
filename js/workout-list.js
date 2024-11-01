14cf0a03bb0b24779c9d61f401870e9cc11fe9b6
Breno Cupertino <brenocuper+fep@gmail.com>
2024-11-01 19:40:06 -0300

    feat: add funcao abre e fecha do menu

--- js/workout-list.js
+++ js/workout-list.js
@@ -1,3 +1,31 @@
 import { authService } from './services/auth.service.js';
 
 authService.routeGuard()
+
+const menuTemplate = `
+  <div id="menuContainer" class="menu-container">
+      <button id="closeMenuButton" class="close-menu-button">
+          <img src="./assets/close-menu-icon.svg" alt="Botão fechar menu">
+      </button>
+
+      <div class="menu-content">
+          <a href="/workout-list.html">Treinos</a>
+          <a href="/create-workout.html">Novo treino</a>
+          <a href="#">Sair</a>
+      </div>
+  </div>
+`
+
+document.querySelector('#openMenuButton').addEventListener('click', handleOpenMenu)
+
+function handleOpenMenu() {
+
+  document.body.insertAdjacentHTML('beforeend', menuTemplate)
+
+  document.querySelector('#closeMenuButton').addEventListener('click', handleCloseMenu)
+  
+}
+
+function handleCloseMenu() {
+  document.querySelector('#menuContainer').remove()
+}
