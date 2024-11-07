6c8ec31ba47d1c4ecfc6d22da66b2d36e572d610
Breno Cupertino <brenocuper+fep@gmail.com>
2024-11-06 21:20:54 -0300

    feat: add js base para listagem de treinos

--- js/workout-list.js
+++ js/workout-list.js
@@ -1,9 +1,30 @@
 import { authService } from './services/auth.service.js'
 import './utils/header.js'
 
-authService.routeGuard()
+document.addEventListener('DOMContentLoaded', initPage)
 
-document.querySelector('#createWorkoutButton').addEventListener('click', handleNavigateToCreateWorkout)
+function initPage() {
+
+  authService.routeGuard()
+
+  const HTMLElements = getHTMLElements()
+
+  setupEventListeners(HTMLElements)
+}
+
+function getHTMLElements() {
+  return {
+    navigateToCreateWorkoutButton: document.querySelector('#createWorkoutButton'),
+    workoutListContainer: document.querySelector('#workoutListContainer')
+  }
+}
+
+function setupEventListeners(HTMLElements) {
+
+  const { navigateToCreateWorkoutButton } = HTMLElements
+
+  navigateToCreateWorkoutButton.addEventListener('click', handleNavigateToCreateWorkout)
+}
 
 function handleNavigateToCreateWorkout() {
   location.href = `/create-workout.html`
